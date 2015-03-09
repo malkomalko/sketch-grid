eb7c34d4b010a8e24591ef8789bf599cc0fc78da
Robert Malko <robmalko@gmail.com>
2015-03-09 18:53:38 -0400

    added some more ui helpers; rename to Make grid

--- library/ui.js
+++ library/ui.js
@@ -1,7 +1,37 @@
-ui.alert = function (msg, title) {
+function alertWindow(msg, title, canCancel, accessoryViews) {
   title = title || '';
-  var app = [NSApplication sharedApplication];
-  [app displayDialog:msg withTitle:title];
+  accessoryViews = accessoryViews || [];
+
+  var modal = COSAlertWindow.new();
+  var icon = NSImage.alloc().initByReferencingFile(
+    pluginPath + '/assets/icon.png');
+
+  modal.setMessageText(title);
+  modal.setInformativeText(msg);
+  modal.setIcon(icon);
+
+  if (canCancel) {
+    modal.addButtonWithTitle('Continue');
+    modal.addButtonWithTitle('Cancel');
+  } else {
+    modal.addButtonWithTitle('OK')
+  }
+
+  accessoryViews.forEach(function (v) {
+    modal.addAccessoryView(v);
+  });
+
+  return modal
+};
+
+function valueAtIndex(view, index) {
+  return view.viewAtIndex(index).stringValue()
+};
+
+ui.alert = function (msg, title) {
+  var modal = alertWindow(msg, title);
+
+  return modal.runModal();
 };
 
 ui.createMenu = function (message, items, selectedItemIndex) {
@@ -11,14 +41,21 @@ ui.createMenu = function (message, items, selectedItemIndex) {
   [accessory addItemsWithObjectValues:items];
   [accessory selectItemAtIndex:selectedItemIndex];
 
-  var alert = [[NSAlert alloc] init];
-  [alert setMessageText:message];
-  [alert addButtonWithTitle:'OK'];
-  [alert addButtonWithTitle:'Cancel'];
-  [alert setAccessoryView:accessory];
-
-  var responseCode = [alert runModal];
+  var modal = alertWindow(message, '', true, [accessory]);
+  var responseCode = modal.runModal();
   var sel = [accessory indexOfSelectedItem];
+  return [responseCode == 1000, sel];
+};
 
-  return [responseCode, sel];
+ui.createTextField = function (message, title, initial) {
+  initial = initial || '';
+
+  var accessory = NSTextField.alloc().initWithFrame(NSMakeRect(0, 0, 250, 150));
+  accessory.setFont(NSFont.fontWithName_size('Monaco', 12));
+  accessory.enablesReturnKeyAutomatically = false;
+  accessory.stringValue = initial;
+
+  var modal = alertWindow(message, title, true, [accessory]);
+  var responseCode = modal.runModal();
+  return [responseCode == 1000, valueAtIndex(modal, 0)];
 };
